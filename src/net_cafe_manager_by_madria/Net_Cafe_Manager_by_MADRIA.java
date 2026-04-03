278fc81d3e6cfb2fbecaa46ee5f5fc0b5bd4dfec
ItsJennyMadria <ihopeyougetbanzaid@gmail.com>
2026-04-03 22:01:14 +0800

    Initial commit: Internet Cafe Management System project structure

---
+++ src/net_cafe_manager_by_madria/Net_Cafe_Manager_by_MADRIA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package net_cafe_manager_by_madria;
+
+/**
+ *
+ * @author Dell
+ */
+public class Net_Cafe_Manager_by_MADRIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
